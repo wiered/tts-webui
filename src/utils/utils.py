70a317bee370b27f1be3f216fac86f5c8c449218
Artem <wieredsh@gmail.com>
2024-09-26 13:47:06 +0300

    added utils and salt

---
+++ src/utils/utils.py
@@ -0,0 +1,7 @@
+﻿import random
+import string
+
+
+def generateSalt(length=16):
+    alphabet = string.ascii_letters + string.digits
+    return ''.join(random.choice(alphabet) for _ in range(length))
