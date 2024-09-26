370140db85822e7c6293851f2306d236a2f80ab9
Artem <wieredsh@gmail.com>
2024-09-26 13:47:54 +0300

    Initial TTS Generator

--- src/audio/tts_gen.py
+++ src/audio/tts_gen.py
@@ -1,3 +1,26 @@
-﻿class TTSGen():
-    def __init__(self):
-        pass
+﻿import io
+
+from gtts import gTTS
+
+from utils import utils
+
+
+class TTSGen():
+    def __init__(self, language: str = "en"):
+        self.language = language
+
+    def generateFromText(self, text) -> io.BytesIO:
+        sound_fp = io.BytesIO()
+        gTTSobj = gTTS(text=text, lang=self.language, slow=False)
+        gTTSobj.write_to_fp(sound_fp)
+        sound_fp.seek(0)
+
+        return sound_fp
+
+    def generateFileFromText(self, text):
+        salt = utils.generateSalt()
+        filename = f"sounds/{salt}tts.mp3"
+        gTTSobj = gTTS(text=text, lang=self.language, slow=False)
+        gTTSobj.save(filename)
+
+        return filename
